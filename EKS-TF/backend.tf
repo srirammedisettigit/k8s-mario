705481e56f27ea2a9d1b4bb7ea053a48695e1fbb
srirammedisettigit <144312502+srirammedisettigit@users.noreply.github.com>
2024-09-26 12:47:45 +0530

    Update backend.tf

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "projectsriram" # Replace with your actual S3 bucket name
+    bucket = "projectsri1234" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-west-1"
   }
